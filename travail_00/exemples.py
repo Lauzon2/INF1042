0b4682e4a6dc7b5a1536dc5fab2c37809b074367
Mark Lessard <nlessard@mon-avenir.ca>
2026-02-03 15:13:29 -0500

    init

---
+++ travail_00/exemples.py
@@ -0,0 +1,3 @@
+valeur = int(input("Donnez une valeur entier: "))
+valeur += 10
+print("La nouvelle valeur est : " + str(valeur))    
---
+++ travaux/travail_00/exemples.py
@@ -0,0 +1,15 @@
+def exemple():
+    a = [1,2,3,4,5]
+    print (2 in a)
+    print (7 in a)
+
+exemple()
+
+def entree():
+    # nom = input("Entrez votre nom : ")
+    # print("Bonjour, " + nom + "!")
+    valeur = int(input("Donnez une valeur entier: "))
+    valeur += 10
+    print("La nouvelle valeur est : " + str(valeur))    
+
+entree()
